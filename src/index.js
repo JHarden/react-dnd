02be8074be066c45f191ae5cf9a072769d8bbfcc
John Harden <jharden1@central1.com>
2018-08-09 15:22:42 -0700

    [DnD] setting up redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import rootReducer from './state/reducers'
+import todoApp from './state/reducers'
+import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } from './state/actions';
 
-const store = createStore(rootReducer)
+const store = createStore(todoApp)
+
+console.log(store.getState())
+
 const Index = () => <div>​Hello React!</div>
+
+// Log the initial state
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+const unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+)
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+// Stop listening to state updates
+unsubscribe()
 
 render(
   <Provider store={store}>
--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -1,7 +1,33 @@
-let nextTodoId = 0
+/*
+ * action types
+ */
 
-export const addTodo = text => ({
-    type: 'ADD_TODO',
-    id: nextTodoId++,
-    text
-})
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+/*
+ * other constants
+ */
+
+export const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+/*
+ * action creators
+ */
+
+export function addTodo(text) {
+    return { type: ADD_TODO, text }
+}
+
+export function toggleTodo(index) {
+    return { type: TOGGLE_TODO, index }
+}
+
+export function setVisibilityFilter(filter) {
+    return { type: SET_VISIBILITY_FILTER, filter }
+}
