da4efb975e8599a329551869dcafdbc63bb58692
John Harden <jharden1@central1.com>
2018-08-10 16:49:57 -0700

    [DnD] - styled components

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,19 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import Styled from 'styled-components'
+
+const Wrapper = Styled.div`
+  border: 1px solid red;
+  background-color: grey;
+`;
 
 const App = () => (
-  <div>
+  <Wrapper>
     <AddTodo />
     <VisibleTodoList />
     <Footer />
-  </div>
+  </Wrapper>
 )
 
 export default App
