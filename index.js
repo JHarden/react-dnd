1c69caa3446e1fdaf74dc9b3761455c99b667e6a
John Harden <jharden1@central1.com>
2018-09-12 21:36:56 -0700

    [DnD] - first endpoint

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ express()
   .use(express.static(path.join(__dirname, 'dist')))
   .set('view engine', 'html')
   .get('/', (req, res) => res.render('index'))
+  .get("/api/ping", (req, res) => {
+    res.status(200).json("Welcome to our restful API");
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,58 @@ import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 import Styled from 'styled-components'
 import StatBlock from './statblock/StatBlock';
+import {Observable} from 'rxjs';
+
 
 const Wrapper = Styled.div`
   border: 1px solid red;
   background-color: grey;
 `;
 
-const App = () => (
-  <Wrapper>
-    <AddTodo />
-    <VisibleTodoList />
-    <StatBlock/>
-    <Footer />
-  </Wrapper>
-)
+class App extends React.Component {
+
+	load(url) {
+		return Observable.create(observer => {
+			let xhr = new XMLHttpRequest();
+
+			let onload = () => {
+				if (xhr.status === 200) {
+					let data = JSON.parse(xhr.responseText);
+					observer.next(data);
+					observer.complete();
+				} else {
+					observer.error(xhr.statusText);
+				}
+			};
+			xhr.addEventListener('load', onload);
+
+			xhr.open('GET', url);
+			xhr.send();
+
+			return () => {
+				xhr.removeEventListener('load', onload);
+				xhr.abort();
+			};
+		});
+	}
+
+	render() {
+
+		this.load('/api/ping').subscribe(val => {
+			console.log('value: ', val);
+		});
+
+		console.log('RENDER');
+
+		return (
+			<Wrapper>
+				<AddTodo />
+				<VisibleTodoList />
+				<StatBlock />
+				<Footer />
+			</Wrapper>
+		)
+	}
+}
 
 export default App
