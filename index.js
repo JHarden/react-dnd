956a75e0b33c3e26dcb3529c8e362ea56ac84948
John Harden <jharden1@central1.com>
2018-09-14 16:02:57 -0700

    [DnD] - test table get

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: false
 });
 
 express()
@@ -18,12 +18,12 @@ express()
   .get('/api/db', async (req, res) => {
     try {
       const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table')
+      const result = await client.query('SELECT * FROM legal')
       const results = { 'results': (result) ? result.rows : null }
       res.json({ results })
       client.release()
     } catch (err) {
-      console.err(err)
+      console.error(err)
       res.send('Error' + err)
     }
   })
